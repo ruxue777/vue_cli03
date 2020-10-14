ceb4e83f3cdcdc35c734cb40d2fa2fc202936ef4
Ruxue <1078134231@qq.com>
2020-10-14 22:23:44 +0800

    导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
@@ -21,7 +26,27 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  mode:'history',
   routes
+})
+
+let falg = true
+
+//全局前置守卫,拦截路由
+router.beforeEach((to,from,next)=>{
+  console.log('open Login...');
+  if(falg){
+    if(to.name === 'Login') next('/')
+    else next()
+  }else{
+    if(to.name === 'Login') next()
+    else next('/login')
+  }
+})
+
+//全局后置钩子
+router.afterEach((to,from)=>{
+  console.log('close Login...');
 })
 
 export default router
