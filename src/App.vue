534f93bb4def1a1675d8f2355bd3ec0424483511
Ruxue <1078134231@qq.com>
2020-10-16 17:01:34 +0800

    转场动画效果

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,32 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
 
-      <br><br>
-
-      <router-link to="/about/51">about51</router-link> |
-      <router-link to="/about/52">about52</router-link>
     </div>
-    <router-view/>
+    <transition :name="tName">
+      <router-view/>
+    </transition>
+    
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      tName: ''
+    }
+  },
+  watch :{
+    '$route'(to,from){
+      if(to.name === 'Home'){
+        this.tName = 'fade'
+      }else if(to.name === 'About'){
+        this.tName = 'fade2'
+      }
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -33,5 +51,29 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.fade-enter-active,.fade-leave-active{
+  transition: opacity .3s;
+}
+
+.fade-enter,.fade-leave-to{
+  opacity: 0;
+  
+}
+.fade-leave-to{
+  display: none;
+}
+
+.fade2-enter-active,.fade2-leave-active{
+  transition: opacity 2s;
+}
+
+.fade2-enter,.fade2-leave-to{
+  opacity: 0;
+  
+}
+.fade2-leave-to{
+  display: none;
 }
 </style>
